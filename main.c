5a90ac8b045a23b6c2e87b3661e441a32a58b20f
tux-pengu1n <P3ngu1nn@protonmail.com>
2025-09-24 12:36:01 +0200

    schönere ausgabe & readme

--- main.c
+++ main.c
@@ -85,32 +85,34 @@ int fits(int i, int j, card field[3][3]) {
   return fit;
 }
 
-void printfield(card f[3][3]) {
+void printfield(card f[3][3], int colors[5]) {
+
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       card temp = f[i][j];
-      printf("   %d    ", temp.o);
+      printf("\033[1;%dm   %3d    ", colors[abs(temp.o)], temp.o);
     }
     printf("\n");
     for (int j = 0; j < 3; j++) {
       card temp = f[i][j];
-      printf("%d     %d ", temp.l, temp.r);
+      printf("\033[1;%dm%3d   \033[1;%dm%3d ", colors[abs(temp.l)], temp.l,
+             colors[abs(temp.r)], temp.r);
     }
     printf("\n");
     for (int j = 0; j < 3; j++) {
       card temp = f[i][j];
-      printf("   %d    ", temp.u);
+      printf("\033[1;%dm   %3d    ", colors[abs(temp.u)], temp.u);
     }
-    printf("\n");
+    printf("\033[1;0m\n");
   }
   printf("-----------------------------------------------------\n");
 }
 
 // brute force
 //
-void brute_force(int current_i, int current_j, card field[3][3],
-                 card cards[9]) {
-  printf("aufgerufen auf %d|%d\n", current_i, current_j);
+void brute_force(int current_i, int current_j, card field[3][3], card cards[9],
+                 int colors[5]) {
+  //  printf("aufgerufen auf %d|%d\n", current_i, current_j);
   int i, j, k, karten_nummer;
   karten_nummer = 0;
   for (i = 0; i < 9; i++) {
@@ -126,14 +128,14 @@ void brute_force(int current_i, int current_j, card field[3][3],
 
     for (j = 0; j < 5; j++) {
       rotate(&field[current_i][current_j]);
-      printfield(field);
-      //      sleep(1);
+      printfield(field, colors);
+      usleep(10000);
       if (fits(current_i, current_j, field)) {
-        printf("yay\n");
+        //        printf("yay\n");
         if (current_i < 2) {
-          brute_force(current_i + 1, current_j, field, cards);
+          brute_force(current_i + 1, current_j, field, cards, colors);
         } else if (current_j < 2) {
-          brute_force(0, current_j + 1, field, cards);
+          brute_force(0, current_j + 1, field, cards, colors);
         } else {
           printf("fertig");
           exit(1);
@@ -141,7 +143,7 @@ void brute_force(int current_i, int current_j, card field[3][3],
       }
     }
   }
-  printf("memnsch");
+  //  printf("memnsch");
   field[current_i][current_j].l = 0;
   field[current_i][current_j].r = 0;
   field[current_i][current_j].o = 0;
@@ -153,13 +155,18 @@ void brute_force(int current_i, int current_j, card field[3][3],
 // feld ausgeben
 
 int main(void) {
+  int colors[5];
+  colors[0] = 30;
+  colors[1] = 33;
+  colors[2] = 31;
+  colors[3] = 32;
+  colors[4] = 36; // should be brown
   card field[3][3] = {0};
-
   card cards[9];
   initcards(cards);
   //  rotate(&cards[1]);
 
-  brute_force(0, 0, field, cards);
+  brute_force(0, 0, field, cards, colors);
 
   int placed1 = 0;
 
